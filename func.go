9f0196ba63ab9a3e1bab8f873e0dd4d02cc31deb
Frank Wang <frankwdy11@gmail.com>
2022-02-16 14:01:03 +0800

    panicfc

--- func.go
+++ func.go
@@ -111,7 +111,7 @@ func TransactionWithSession(ctx interface{}, sess *xorm.Session, handlers ...Tra
 		// 3.1 Catch panic.
 		if r := recover(); r != nil {
 			err = errors.New(fmt.Sprintf("%v", r))
-			log.Errorfc(ctx, "[SQL] transaction panic: %s.", err.Error())
+			log.Panicfc(ctx, "[SQL] transaction panic: %s.", err.Error())
 		}
 		// 3.2 End transaction.
 		if err != nil {
